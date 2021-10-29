6c26083bb9297b93431c0a88c9b808b45175e30f
MANI <92910524+manikanta953@users.noreply.github.com>
2021-10-28 23:59:43 -0700

    Add files via upload

---
+++ variable and outputs.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	// variable declartion
+	
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age=17;
+	cgpa=8.0;
+	initial='M';
+	
+	//output
+	printf("Age of the student is %d\n", age);
+	printf("cgpa of the student is %f\n", cgpa);
+	printf("student surname start is %c", initial);
+	
+}
