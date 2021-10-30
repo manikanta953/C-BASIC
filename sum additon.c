9b3d8e15ad53a24a7245f5143e1e429f6f62c89b
MANI <92910524+manikanta953@users.noreply.github.com>
2021-10-30 11:43:02 +0530

    Add files via upload

---
+++ sum additon.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variables
+	int num1, num2, sum;
+	
+	//initialization
+	num1=123;
+	num2=456;
+	
+	//calculation
+	sum=num1+num2;
+	
+	//output
+	printf("%d",sum);
+}
